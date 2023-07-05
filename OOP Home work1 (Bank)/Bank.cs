f0fb00bd7f2065d062d87c8ee85ee0714b837d3e
alexSh14 <alex.1486@mail.ru>
2023-07-05 23:34:35 +0700

    Добавьте файлы проекта.

---
+++ OOP Home work1 (Bank)/Bank.cs
@@ -0,0 +1,111 @@
+﻿using System;
+namespace OOP;
+
+
+class BankAccount
+{
+    private string owner;
+    private float balance;
+
+    public BankAccount(string owner, float balance)
+    {
+        this.owner = owner;
+        this.balance = balance;
+    }
+
+    public float GetBalance()
+    {
+        return balance;
+    }
+
+    public string GetOwner()
+    {
+        return owner;
+    }
+
+    public void Transfer(BankAccount toAccount, float amount)
+    {
+        if (amount > 0 && amount <= balance)
+        {
+            balance -= amount;
+            toAccount.balance += amount;
+        }
+    }
+}
+
+class BankTransaction
+{
+    private BankAccount sender;
+    private BankAccount recipient;
+    private float amount;
+
+    public BankTransaction(BankAccount sender, BankAccount recipient, float amount)
+    {
+        this.sender = sender;
+        this.recipient = recipient;
+        this.amount = amount;
+    }
+
+    public void Execute()
+    {
+        sender.Transfer(recipient, amount);
+    }
+}
+
+class Bank
+{
+    private List<BankAccount> accounts;
+
+    public Bank()
+    {
+        accounts = new List<BankAccount>();
+    }
+
+    public BankAccount AddAccount(string owner, float balance)
+    {
+        BankAccount account = new BankAccount(owner, balance);
+        accounts.Add(account);
+        return account;
+    }
+
+    public float GetAccountBalance(string owner)
+    {
+        foreach (BankAccount account in accounts)
+        {
+            if (account.GetOwner() == owner)
+            {
+                return account.GetBalance();
+            }
+        }
+        return -1;  // Account not found
+    }
+
+    public List<BankAccount> GetAllAccounts()
+    {
+        return accounts;
+    }
+
+    public void TransferFunds(string fromOwner, string toOwner, float amount)
+    {
+        BankAccount fromAccount = null;
+        BankAccount toAccount = null;
+
+        foreach (BankAccount account in accounts)
+        {
+            if (account.GetOwner() == fromOwner)
+            {
+                fromAccount = account;
+            }
+            else if (account.GetOwner() == toOwner)
+            {
+                toAccount = account;
+            }
+        }
+
+        if (fromAccount != null && toAccount != null)
+        {
+            BankTransaction transaction = new BankTransaction(fromAccount, toAccount, amount);
+            transaction.Execute();
+        }
+    }
+} 
---
+++ OOP Home work1 (Bank)/Program.cs
@@ -0,0 +1,13 @@
+﻿using OOP;
+using System;
+using System.Collections.Generic;
+
+            Bank bank = new Bank();
+
+            BankAccount account1 = bank.AddAccount("John", 500);
+            BankAccount account2 = bank.AddAccount("Jane", 1000);
+
+            bank.TransferFunds("John", "Jane", 200);
+
+            Console.WriteLine("John's balance: " + bank.GetAccountBalance("John"));
+            Console.WriteLine("Jane's balance: " + bank.GetAccountBalance("Jane"));
